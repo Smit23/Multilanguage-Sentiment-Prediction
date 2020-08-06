0688a8a515e85b11b9f62f32fc46ccbf8b42ceb3
Smit23 <44703480+Smit23@users.noreply.github.com>
2020-08-05 20:22:58 -0400

    Initial Commit

---
+++ Code/char_rnn_test.py
@@ -0,0 +1,57 @@
+from keras.models import model_from_json
+from keras.utils import np_utils
+import numpy as np
+import h5py
+import pickle
+from copy import deepcopy
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+
+#Masterdir = '/media/ameya/Research/Sub-word-LSTM/'
+Masterdir = 'C:/Users/smit2/OneDrive/Desktop/Sub-word-LSTM-master/'
+Datadir = 'Data/'
+Modeldir = 'Pretrained_models/'
+Featuredir = 'Features/'
+inputdatasetfilename = 'IIITH_Codemixed.txt'
+experiment_details = 'lstm128_subword'
+filename = 'match.txt'
+batch_size = 128
+numclasses = 3
+charrnndir='Saved_'
+
+def accuracy(original, predicted):
+	print("F1 score is: " + str(f1_score(original, predicted, average='macro')))
+	scores = confusion_matrix(original, predicted)
+	print (scores)
+	print( np.trace(scores)/float(np.sum(scores)))
+
+h5f = h5py.File(Masterdir+Datadir+'Xtest_'+experiment_details+'.h5','r')
+X_test = h5f['dataset'][:]
+h5f.close()
+inp = open(Masterdir+Datadir+'Ytest_'+experiment_details+'.pkl', 'rb')
+y_test=pickle.load(inp)
+inp.close()
+y_test=np.asarray(y_test).flatten()
+y_test2 = np_utils.to_categorical(y_test, numclasses) 
+print(y_test.shape)
+print("qwertyuiop",Masterdir+Modeldir+'LSTM_'+experiment_details+'_architecture.json')
+f = open(Masterdir+'Saved_Models/'+'LSTM_'+'new_experiment'+'_architecture.json','r+')
+json_string = f.read()
+f.close()
+
+models = model_from_json(json_string)
+
+models.load_weights(Masterdir+'Saved_Models/'+'LSTM_'+'new_experiment'+'_weights.h5')
+models.compile(loss='categorical_crossentropy', optimizer='adamax', metrics=['accuracy'])
+	
+#X_test = "Its awesome"
+score, acc = models.evaluate(X_test, y_test2, batch_size=batch_size)
+
+y_pred = models.predict_classes(X_test, batch_size=batch_size)
+#print(y_pred)
+accuracy(y_test,y_pred)
+
+print('Accuracy is: '+str(acc))
